509187592faa4e24051bc459e8e469c89c24c41c
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-10-09 19:04:18 -0700

    Move static files to www folder

--- staticServer.js
+++ staticServer.js
@@ -1,11 +1,8 @@
 var http = require('http');
 var ecstatic = require('ecstatic');
 
-var staticRoot = './';
+var staticRoot = 'www';
 var port = process.env.PORT || 4400;
 
 http.createServer(ecstatic({root: staticRoot})).listen(port);
-console.log("HTTP server listening on " + port);
-
-
-// console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+console.log("HTTP server listening on\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
